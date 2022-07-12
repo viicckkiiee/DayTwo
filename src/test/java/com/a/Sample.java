59dc4adb1b2803411763810bbc70102f7b900786
viicckkiiee <vigneshvaidegi@gmail.com>
2022-07-12 22:44:56 +0530

    1st edition

---
+++ src/test/java/com/a/Sample.java
@@ -0,0 +1,31 @@
+package com.a;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+		@Test
+		private void test1() {
+		System.out.println("1################################1");	
+		}
+		
+		@Test
+		private void test2() {
+			System.out.println("2################################2");	
+		}
+		
+		@Test
+		private void test3() {
+			System.out.println("3################################3");	
+		}
+		
+		@Test
+		private void test4() {
+			System.out.println("4################################4");	
+		}
+		
+		
+		
+
+	}
+
